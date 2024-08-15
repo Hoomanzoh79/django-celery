cc185d1b95b932be1f348499c080fad588d96e03
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-08-15 17:24:30 +0330

    add celery configs and add_numbers task

--- core/celery.py
+++ core/celery.py
@@ -1,2 +1,12 @@
 import os
 from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+app = Celery("core")
+app.config_from_object("django.conf:settings",namespace="CELERY")
+
+@app.task
+def add_numbers():
+    return 
+
+app.autodiscover_tasks()
