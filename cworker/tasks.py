9a1717245a4855bb6eb8d36a7293ccf846b8955d
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-08-19 10:56:13 +0330

    add 4 tasks

--- cworker/tasks.py
+++ cworker/tasks.py
@@ -1,9 +1,22 @@
 from celery import shared_task
+import time
 
 @shared_task
-def task1():
+def task1(queue='celery'):
+    time.sleep(3)
     print('task1 is done successfully')
 
 @shared_task
-def task2():
+def task2(queue='celery:1'):
+    time.sleep(3)
     print('task2 is done successfully')
+
+@shared_task
+def task3(queue='celery:2'):
+    time.sleep(3)
+    print('task3 is done successfully')
+
+@shared_task
+def task4(queue='celery:3'):
+    time.sleep(3)
+    print('task4 is done successfully')
