973ed128de3420e8311af4ebf0bfc8fd9755c784
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-08-19 10:56:06 +0330

    add tasks priority configs

--- core/celery.py
+++ core/celery.py
@@ -5,7 +5,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 app = Celery("core")
 app.config_from_object("django.conf:settings",namespace="CELERY")
 
-app.conf.task_routes = {'cworker.tasks.task1':{'queue':'queue1'},
-                        'cworker.tasks.task2':{'queue':'queue2'},
-                        }
+# app.conf.task_routes = {'cworker.tasks.task1':{'queue':'queue1'},
+#                         'cworker.tasks.task2':{'queue':'queue2'},
+#                         }
+
+# tasks priority configs
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep':':',
+    'queue_order_strategy':'priority',
+}
+
 app.autodiscover_tasks()
