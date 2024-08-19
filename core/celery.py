5b84e2633bf8378131439f73c31431003de6e551
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-08-19 15:04:17 +0330

    add task rate limit configs

--- core/celery.py
+++ core/celery.py
@@ -1,10 +1,12 @@
 import os
 from celery import Celery
 
+# celery configs
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 app = Celery("core")
 app.config_from_object("django.conf:settings",namespace="CELERY")
 
+# task routing
 # app.conf.task_routes = {'cworker.tasks.task1':{'queue':'queue1'},
 #                         'cworker.tasks.task2':{'queue':'queue2'},
 #                         }
@@ -15,5 +17,9 @@ app.conf.broker_transport_options = {
     'sep':':',
     'queue_order_strategy':'priority',
 }
+
+# tasks rate limit ---> the default value is 10/m 
+# meaning 10 tasks per minute,we set it to 1 task per minute(of the same task)
+app.conf.task_default_rate_limit = '1/m'
 
 app.autodiscover_tasks()
